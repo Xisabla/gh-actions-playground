d525680ab4f524422fb8fcba60e465d2c19626b9
Gautier Miquet <xisabla.dev@gmail.com>
2020-06-12 16:55:11 +0200

    example code

---
+++ src/Application.ts
@@ -0,0 +1,17 @@
+export default class Application {
+ 
+    private users: string[]
+
+    constructor(users: string[] = []) {
+        this.users = users
+    }
+
+    addUser(user: string): void {
+        this.users.push(user)
+    }
+
+    greet(): void {
+        this.users.forEach((user) => console.log(`Hello ${user}`))
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import Application from './Application';
+
+const app = new Application()
+
+app.addUser('Foo')
+app.addUser('Bar')
+
+app.greet()
